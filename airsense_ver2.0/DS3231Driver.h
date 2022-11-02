17626f50eec13e6b2782796d9a2f852005e88b45
NgDKhanh <Khanh1piecesky@gmail.com>
2022-11-02 21:47:12 +0700

    Upadated DS3231Driver.h & MQ131Driver.h

--- airsense_ver2.0/DS3231Driver.h
+++ airsense_ver2.0/DS3231Driver.h
@@ -1,20 +1,26 @@
+#pragma once
 #include "RTClib.h"
 #include <ESP32Ping.h>
+#include "MQTTConnection.h"
 #include "log.h"
 
-/* DS3231 init status */
-#define DS3231_INIT_SUCCESS		0x30
-#define DS3231_INIT_UNSUCCESS	0x31
 
-const char* remote_host_string = "www.google.com";
+/* DS3231 init status */
+#define DS3231_INIT_SUCCESS			0x70
+#define DS3231_INIT_UNSUCCESS		0x71
+#define DS3231_GETDATA_SUCCESS		0x72
+#define DS3231_GETDATA_UNSUCCESS	0x73
+
+const char *remote_host_string = "www.google.com";
 RTC_DS3231 realTime;
 
 /**
  * @brief	Khoi tao module thoi gian thuc
  *
- * @return  None
+ * @return  DS3231_INITSUCCESS		0x70: Khoi tao DS3231 thanh cong
+ * @return  DS3231_INITUNSUCCESS	0x71: Khoi tao DS3231 khong thanh cong
  */
-void DS3231_init()
+uint32_t DS3231_init()
 {
 	realTime.begin();									// khoi dong module RTC
 	if (WiFi.status() == wl_status_t::WL_CONNECTED)		// kiem tra co ket noi wifi
@@ -26,7 +32,7 @@ void DS3231_init()
 			uint32_t epochTime_u32 = timeClient.getEpochTime();
 			realTime.adjust(DateTime(epochTime_u32));			// Set the date and flip the Oscillator Stop Flag
 			LOG_PRINT_NOTIFICATION("Updatetime DS3231....");
-			return DS3231_INIT_SUCCESS;					// Khoi tao DS3231 thaanh cong
+			return DS3231_INIT_SUCCESS;					// Khoi tao DS3231 thanh cong
 		}
 	}
 	return DS3231_INIT_UNSUCCESS;						// Khoi tao DS3231 khong thanh cong
@@ -40,8 +46,8 @@ void DS3231_init()
 /**
  * @brief	luu tru thoi gian thuc vao bien TFT_time_string
  *
- * @return true 	neu lay duoc thoi gian hop le
- * @return false 	neu thoi gian khong hop le
+ * @return DS3231_GETDATA_SUCCESS	0x72 : neu lay duoc thoi gian hop le
+ * @return DS3231_GETDATA_UNSUCCESS	0x73 :neu thoi gian khong hop le
  */
 bool DS3231_getData()
 {
@@ -50,11 +56,11 @@ bool DS3231_getData()
 #ifdef  DEBUG_SERIAL
 	LOG_PRINT_NOTIFICATION(getDateTime("hh:mm DD-MMM-YY"));		// in thoi gian hien tai ra Serial
 #endif
-		return true;
+		return DS3231_GETDATA_SUCCESS;
 	} else {
 #ifdef  DEBUG_SERIAL
 	LOG_PRINT_ERROR("------ *** CAN'T GET TIME *** -----");
 #endif
-		return false;
+		return DS3231_GETDATA_UNSUCCESS;
 	}
 }
--- airsense_ver2.0/MQ131Driver.h
+++ airsense_ver2.0/MQ131Driver.h
@@ -1,52 +1,66 @@
 #ifdef O3_SENSOR_MQ131
 #include <MQ131.h>
 
-#define MQ131_INIT_SUCCESS   0x40
-#define MQ131_INIT_UNSUCCESS 0x41
+#define MQ131_INIT_SUCCESS   0x60
+#define MQ131_INIT_UNSUCCESS 0x61
 
 /**
  * @brief	Khoi tao module MQ131
  *
- * @return  MQ131_INIT_SUCCESS   0x40 : khoi dong sensor MQ131 thannh cong
- * @return  MQ131_INIT_UNSUCCESS 0x41: khoi dong sensor MQ131 khong thannh cong
+ * @return  MQ131_INIT_SUCCESS   0x60 : khoi dong sensor MQ131 thannh cong
+ * @return  MQ131_INIT_UNSUCCESS 0x61 : khoi dong sensor MQ131 khong thannh cong
  */
 uint32_t O3_init()
 {
 // MQ131.begin(2,4, LOW_CONCENTRATION, 1000000);  
 	MQ131.begin(PIN_MQ131_POWER, PIN_MQ131_SENSOR, MQ131Model::LOW_CONCENTRATION, MQ131_DEFAULT_RL);  	// khoi dong sensor  MQ131
-	LOG_PRINT_ERROR("Calibration in progress...");
+	LOG_PRINT_NOTIFICATION("Calibration in progress...");
 	MQ131.calibrate();		//Calibrate the basic values
 #ifdef	DEBUG_SERIAL
-	LOG_PRINT_ERROR("Calibration done!");
-	LOG_PRINT_ERROR("R0 = "+String(MQ131.getR0())+" Ohms");
-	LOG_PRINT_ERROR("Time to heat = "+String(MQ131.getTimeToRead())+" s");
-	return MQ131_INIT_SUCCESS;		// khoi dong sensor MQ131 thannh cong
+	LOG_PRINT_NOTIFICATION("Calibration done!");
+	LOG_PRINT_NOTIFICATION("R0 = "+String(MQ131.getR0())+" Ohms");
+	LOG_PRINT_NOTIFICATION("Time to heat = "+String(MQ131.getTimeToRead())+" s");
 #endif
-	return MQ131_INIT_UNSUCCESS;	// khoi dong sensor MQ131 khong thannh cong
+	if(MQ131.getO3(MQ131Unit::PPB) != 0.0)	// kiem tra co gia tri Rs duoc doc(read) khong
+		return MQ131_INIT_SUCCESS;		// khoi dong sensor MQ131 thannh cong
+	else
+		return MQ131_INIT_UNSUCCESS;	// khoi dong sensor MQ131 khong thannh cong
 }
 
 
 
 /**
- * @brief	Luu tru gia tri vao bien TFF_o3_ppm, TFT_o3_ug
- *
- * @return  None
+ * @brief	Luu tru gia tri vao bien O3_dislay_ppm, O3_dislay_ug
+ * 
+ * @param   O3_dislay_ppb_u32	gia tri O3 tren man hinh don vi ppb
+ * @param   O3_min_ppb_u32		gia tri O3 nho nhat don vi ppb
+ * @param   O3_max_ppb_u32		gia tri O3 lon nhat don vi ppb
+ * @param   O3_display_ppm		gia tri O3 tren man hinh don vi ppm
+ * @param   O3_display_ug		gia tri O3 tren man hinh don vi ug
+ * @param   O3_min_ug			gia tri O3 nho nhat don vi ug
+ * @param   O3_min_ppm			gia tri O3 nho nhat don vi ppm
+ * @param   O3_max_ug			gia tri O3 lon nhat don vi ug
+ * @param   O3_max_ppm			gia tri O3 lon nhat don vi ppm
+ * 
+ * @return  O3_dislay_ppb_u32
  */
-void O3_getData()
+uint32_t O3_getData(uint32_t *O3_dislay_ppb_u32, uint32_t O3_min_ppb_u32, uint32_t O3_max_ppb_u32, 
+				float *O3_display_ppm, float *O3_display_ug, float *O3_min_ug, float *O3_min_ppm, float *O3_max_ug, float *O3_max_ppm)
 {
-	TFT_o3_ppb_u32 = random(30,46);
+	*O3_dislay_ppb_u32 = random(30,46);
 
 	// lay gia tri min  cua O3 theo don vi ppb
-	min_o3_ppb_u32 = (min_o3_ppb_u32 > TFT_o3_ppb_u32 )	? min_o3_ppb_u32 : TFT_o3_ppb_u32;
+	O3_min_ppb_u32 = (O3_min_ppb_u32 > *O3_dislay_ppb_u32 )	? O3_min_ppb_u32 : TFT_o3_ppb_u32;
 	// lay gia tri max  cua O3 theo don vi ppb
-	max_o3_ppb_u32 = (max_o3_ppb_u32 < TFT_o3_ppb_u32 ) ? max_o3_ppb_u32 : TFT_o3_ppb_u32;
+	O3_max_ppb_u32 = (O3_max_ppb_u32 < *O3_dislay_ppb_u32 ) ? O3_max_ppb_u32 : TFT_o3_ppb_u32;
 
-	TFT_o3_ppm = float(TFT_o3_ppb_u32)/1000;
-	TFT_o3_ug  = float(TFT_o3_ppb_u32)*1.96;
-	min_o3_ug  = float(min_o3_ppb_u32)*1.96;
-	min_o3_ppm = float(min_o3_ppb_u32)/1000;
-	max_o3_ug  = float(max_o3_ppb_u32)*1.96;
-	max_o3_ppm = float(max_o3_ppb_u32)/1000;
+	*O3_display_ppm = float(*O3_dislay_ppb_u32)/1000;
+	*O3_display_ug  = float(*O3_dislay_ppb_u32)*1.96;
+	*O3_min_ug  	= float(O3_min_ppb_u32)*1.96;
+	*O3_min_ppm 	= float(O3_min_ppb_u32)/1000;
+	*O3_max_ug  	= float(O3_max_ppb_u32)*1.96;
+	*O3_max_ppm 	= float(O3_max_ppb_u32)/1000;
+	return *O3_dislay_ppb_u32;		// tra ve gia tri cua O3_display_ppb_u32 de kiem tra trong ham main
 }
 
 #endif
